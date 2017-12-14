93067c6b5b87db7bf3438c1d2d8b06ea7f958d09
quynh <quynh@softworld.jp>
2017-12-14 17:13:54 +0700

    test index

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,19 +3,46 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: 'contact', component: ContactsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '', component: HomeComponent },
+  // { path: '',
+  //   redirectTo: '/home',
+  //   pathMatch: 'full'
+  // },
+  { path: '**', component: PagenotfoundComponent }
+];
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactsComponent
+    ContactsComponent,
+    LoginComponent,
+    RegisterComponent,
+    PagenotfoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  loginUser(formLogin){   
+      console.log(formLogin);
+      //throw new Error('Form is in valid');   
+  }
+
+}
